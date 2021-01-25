5bc2215ff17cced79928cedb4b0a59cf86919248
RashmitaSoni <36473003+RashmitaSoni@users.noreply.github.com>
2021-01-25 19:42:46 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -5,12 +5,13 @@ namespace Statistics
 {
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
+        public boolean CalculateStatistics(List<float> numbers) {
             //Implement statistics here
             float average, min, max;
             min = numbers.Min(); //calculating min
             max = numbers.Max(); //calculating max
             average = numbers.Average(); //calculating average
+            return true;
         }
     }
 }
